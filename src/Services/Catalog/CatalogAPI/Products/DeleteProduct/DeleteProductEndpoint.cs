45c94227cef0644bc6d9222e287fb7c9f8782ea7
MajaharShaikh <mjhr.shaikh@gmail.com>
2025-12-30 10:49:53 +0530

    Catalog api and cross cutting concerns

---
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace CatalogAPI.Products.DeleteProduct
+{
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        //public record DeleteProductRequest(Guid Id);
+        public record DeleteProductResponse(bool IsSuccess);
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}",
+                async (Guid id, ISender sender) =>
+                {
+                    var command = new DeleteProductCommand(id);
+                    var result = await sender.Send(command);
+                    var response = result.Adapt<DeleteProductResponse>();
+                    return Results.Ok(response);
+                })
+                .WithName("DeleteProduct")
+                .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Delete Product")
+                .WithDescription("Delete Product");
+        }
+    }
+}
